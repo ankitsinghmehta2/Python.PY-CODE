92bf60b8f49d509725564bfab380f91dde4f4f61
AnkitMehta <ankitsingmehta12345@gmail.com>
2024-01-17 21:25:43 +0530

    Python code

---
+++ Python PY/CODE/Print.py
@@ -0,0 +1,11 @@
+# Printing a String:
+print("Hello, World!")
+
+
+print("The print() function in Python displays output to the console")
+
+
+# Printing Variables:
+name = "John"
+age = 25
+print("Name:", name, "Age:", age)
---
+++ Python PY/CODE/comment.py
@@ -0,0 +1,8 @@
+# This is a single-line comment
+
+'''
+
+This is a
+multiple-line
+comment in Python.
+'''
