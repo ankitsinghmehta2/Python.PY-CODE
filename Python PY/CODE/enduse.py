6d806fae7039064c829d1ea98d1f7b66e8d28e78
Ankit Mehta <126945410+ankitsinghmehta2@users.noreply.github.com>
2024-01-18 18:47:27 +0530

    Add files via upload

---
+++ Python PY/CODE/enduse.py
@@ -0,0 +1,8 @@
+# The end parameter in Python's print() function sets the character(s) at the statement's end;
+
+#  e.g., 
+
+print("Hello", end='!')
+# output "Hello!" 
+#  end with !
+# without a newline.
---
+++ Python PY/CODE/sep.py
@@ -0,0 +1,8 @@
+# Sep is a parameter in Python that primarily formats the printed statements on the output screen. Whitespace is the default value of this parameter. It adds a separator between strings to be printed. Let us see some examples to make our concept clear.
+
+# sep use for separator
+# examples sep form ,
+print("hello" ," world" ,sep=" ")
+
+# for new line
+print("hello \n world")
