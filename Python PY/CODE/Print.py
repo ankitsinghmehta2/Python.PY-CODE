50865785850d4b833b63628dd5d2a0eda761b083
AnkitMehta <ankitsingmehta12345@gmail.com>
2024-01-18 18:51:01 +0530

    python.py

--- Python PY/CODE/Print.py
+++ Python PY/CODE/Print.py
@@ -6,6 +6,6 @@ print("The print() function in Python displays output to the console")
 
 
 # Printing Variables:
-name = "John"
-age = 25
+name = "ankit"
+age = 19
 print("Name:", name, "Age:", age)
---
+++ Python PY/CODE/inputfromuser.py
@@ -0,0 +1,14 @@
+
+a=input("enter all string number: ")
+
+# int use for number
+# example
+# only use for number not character show error
+a=int(input("enter the number: "))
+print(a)
+
+b=float(input("enter the number in point : "))
+print(b)
+
+c = eval(input("Enter an expression: "))
+print(c)
