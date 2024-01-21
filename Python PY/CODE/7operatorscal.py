72493039af0a56627aa6625826cfd7b079e42cfa
AnkitMehta <ankitsingmehta12345@gmail.com>
2024-01-21 11:21:03 +0530

    example of operater

---
+++ Python PY/CODE/7operatorscal.py
@@ -0,0 +1,10 @@
+# creating calculator useign operaters
+a=int(input("Enter the 1st number : "))
+b=int(input("Enter the 2nd number : "))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
