e1e6327517f4054bbc4234b9fe92da7c76aa8b22
Ankit Mehta <126945410+ankitsinghmehta2@users.noreply.github.com>
2024-02-08 15:03:07 +0530

    Update 10.if_practice.py

--- Python PY/CODE/10.if_practice.py
+++ Python PY/CODE/10.if_practice.py
@@ -3,8 +3,6 @@ if age >= 18:
     print("You are eligible to vote!")
 else:
     print("You are not eligible to vote yet.")
-
-
 num1=float(input("Enter the First number: "))
 num2=float(input("Enter the Second number: "))
 num3=float(input("enter the third number: "))
